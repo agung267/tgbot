6d97e0bab80bc5a516c0abe6631a571d4e5d1db0
agung267 <63548223+agung267@users.noreply.github.com>
2020-07-23 00:20:40 +0700

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,12 +10,12 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "1240989955"
+    OWNER_ID = "1154565101"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "@nlydiawati"
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = 'Postgresql://PCBoT:taikanjing@0.0.0.0:5422/Postgresql'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
     LOAD = []
     # sed has been disabled after the discovery that certain long-running sed commands maxed out cpu usage
